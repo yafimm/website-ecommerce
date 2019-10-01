af61471095f9d19da78ff7cc57e3325753f986b4
Dzaki Madhani <dzaki.madhani@gmail.com>
2019-10-01 18:58:53 +0700

    Add List Produk + Asset on template

---
+++ resources/views/produk/daftar-produk.blade.php
@@ -0,0 +1,298 @@
+@extends('template.template')
+@section('content')
+<!--================Category Product Area =================-->
+<section class="cat_product_area section_gap">
+		<div class="container-fluid">
+			<div class="row flex-row-reverse">
+				<div class="col-lg-9">
+					<div class="product_top_bar">
+						<div class="left_dorp">
+							<select class="show">
+								<option value="1">Show 12</option>
+								<option value="2">Show 24</option>
+								<option value="4">Show 32</option>
+							</select>
+						</div>
+						<div class="right_page ml-auto">
+							<nav class="cat_page" aria-label="Page navigation example">
+								<ul class="pagination">
+									<li class="page-item">
+										<a class="page-link" href="#">
+											<i class="fa fa-long-arrow-left" aria-hidden="true"></i>
+										</a>
+									</li>
+									<li class="page-item active">
+										<a class="page-link" href="#">1</a>
+									</li>
+									<li class="page-item">
+										<a class="page-link" href="#">2</a>
+									</li>
+									<li class="page-item">
+										<a class="page-link" href="#">3</a>
+									</li>
+									<li class="page-item blank">
+										<a class="page-link" href="#">...</a>
+									</li>
+									<li class="page-item">
+										<a class="page-link" href="#">6</a>
+									</li>
+									<li class="page-item">
+										<a class="page-link" href="#">
+											<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+										</a>
+									</li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+					<div class="latest_product_inner row">
+						<div class="col-lg-3 col-md-3 col-sm-6">
+							<div class="f_p_item">
+								<div class="f_p_img">
+									<img class="img-fluid" src="img/product/feature-product/f-p-5.jpg" alt="">
+									<div class="p_icon">
+										<a href="#">
+											<i class="lnr lnr-heart"></i>
+										</a>
+										<a href="#">
+											<i class="lnr lnr-cart"></i>
+										</a>
+									</div>
+								</div>
+								<a href="#">
+									<h4>Long Sleeve TShirt</h4>
+								</a>
+								<h5>$150.00</h5>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-3 col-sm-6">
+							<div class="f_p_item">
+								<div class="f_p_img">
+									<img class="img-fluid" src="img/product/feature-product/f-p-4.jpg" alt="">
+									<div class="p_icon">
+										<a href="#">
+											<i class="lnr lnr-heart"></i>
+										</a>
+										<a href="#">
+											<i class="lnr lnr-cart"></i>
+										</a>
+									</div>
+								</div>
+								<a href="#">
+									<h4>Long Sleeve TShirt</h4>
+								</a>
+								<h5>$150.00</h5>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-3 col-sm-6">
+							<div class="f_p_item">
+								<div class="f_p_img">
+									<img class="img-fluid" src="img/product/feature-product/f-p-3.jpg" alt="">
+									<div class="p_icon">
+										<a href="#">
+											<i class="lnr lnr-heart"></i>
+										</a>
+										<a href="#">
+											<i class="lnr lnr-cart"></i>
+										</a>
+									</div>
+								</div>
+								<a href="#">
+									<h4>Long Sleeve TShirt</h4>
+								</a>
+								<h5>$150.00</h5>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-3 col-sm-6">
+							<div class="f_p_item">
+								<div class="f_p_img">
+									<img class="img-fluid" src="img/product/feature-product/f-p-4.jpg" alt="">
+									<div class="p_icon">
+										<a href="#">
+											<i class="lnr lnr-heart"></i>
+										</a>
+										<a href="#">
+											<i class="lnr lnr-cart"></i>
+										</a>
+									</div>
+								</div>
+								<a href="#">
+									<h4>Long Sleeve TShirt</h4>
+								</a>
+								<h5>$150.00</h5>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-3 col-sm-6">
+							<div class="f_p_item">
+								<div class="f_p_img">
+									<img class="img-fluid" src="img/product/feature-product/f-p-4.jpg" alt="">
+									<div class="p_icon">
+										<a href="#">
+											<i class="lnr lnr-heart"></i>
+										</a>
+										<a href="#">
+											<i class="lnr lnr-cart"></i>
+										</a>
+									</div>
+								</div>
+								<a href="#">
+									<h4>Long Sleeve TShirt</h4>
+								</a>
+								<h5>$150.00</h5>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-3 col-sm-6">
+							<div class="f_p_item">
+								<div class="f_p_img">
+									<img class="img-fluid" src="img/product/feature-product/f-p-3.jpg" alt="">
+									<div class="p_icon">
+										<a href="#">
+											<i class="lnr lnr-heart"></i>
+										</a>
+										<a href="#">
+											<i class="lnr lnr-cart"></i>
+										</a>
+									</div>
+								</div>
+								<a href="#">
+									<h4>Long Sleeve TShirt</h4>
+								</a>
+								<h5>$150.00</h5>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-3 col-sm-6">
+							<div class="f_p_item">
+								<div class="f_p_img">
+									<img class="img-fluid" src="img/product/feature-product/f-p-4.jpg" alt="">
+									<div class="p_icon">
+										<a href="#">
+											<i class="lnr lnr-heart"></i>
+										</a>
+										<a href="#">
+											<i class="lnr lnr-cart"></i>
+										</a>
+									</div>
+								</div>
+								<a href="#">
+									<h4>Long Sleeve TShirt</h4>
+								</a>
+								<h5>$150.00</h5>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="left_sidebar_area">
+						<aside class="left_widgets cat_widgets">
+							<div class="l_w_title">
+								<h3>smartphone category</h3>
+							</div>
+							<div class="widgets_inner">
+								<ul class="list">
+									<li>
+										<a href="#">Samsung</a>
+										<ul class="list">
+											<li>
+												<a href="#">Tipe A</a>
+											</li>
+											<li>
+												<a href="#">Tipe S</a>
+											</li>
+										</ul>
+									</li>
+									<li>
+										<a href="#">Asus</a>
+										<ul class="list">
+											<li>
+												<a href="#">Zenfone Max</a>
+											</li>
+											<li>
+												<a href="#">Zenfone Max Pro</a>
+											</li>
+											<li>
+												<a href="#">Zenfone</a>
+											</li>
+											<li>
+												<a href="#">Zenfone Live</a>
+											</li>
+											<li>
+												<a href="#">ROG</a>
+											</li>
+										</ul>
+									</li>
+									<li>
+										<a href="#">Oppo</a>
+										<ul class="list">
+											<li>
+												<a href="#">Tipe F</a>
+											</li>
+											<li>
+												<a href="#">Tipe A</a>
+											</li>
+										</ul>
+									</li>
+									<li>
+										<a href="#">Vivo</a>
+										<ul class="list">
+											<li>
+												<a href="#">Tipe F</a>
+											</li>
+											<li>
+												<a href="#">Tipe A</a>
+											</li>
+										</ul>
+									</li>
+									<li>
+										<a href="#">Xiaomi</a>
+										<ul class="list">
+											<li>
+												<a href="#">Tipe F</a>
+											</li>
+											<li>
+												<a href="#">Tipe A</a>
+											</li>
+										</ul>
+									</li>
+								</ul>
+							</div>
+						</aside>
+						
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<nav class="cat_page mx-auto" aria-label="Page navigation example">
+					<ul class="pagination">
+						<li class="page-item">
+							<a class="page-link" href="#">
+								<i class="fa fa-chevron-left" aria-hidden="true"></i>
+							</a>
+						</li>
+						<li class="page-item active">
+							<a class="page-link" href="#">01</a>
+						</li>
+						<li class="page-item">
+							<a class="page-link" href="#">02</a>
+						</li>
+						<li class="page-item">
+							<a class="page-link" href="#">03</a>
+						</li>
+						<li class="page-item blank">
+							<a class="page-link" href="#">...</a>
+						</li>
+						<li class="page-item">
+							<a class="page-link" href="#">09</a>
+						</li>
+						<li class="page-item">
+							<a class="page-link" href="#">
+								<i class="fa fa-chevron-right" aria-hidden="true"></i>
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</section>
+	<!--================End Category Product Area =================-->
+@endsection    
--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -8,17 +8,17 @@
 	<link rel="icon" href="img/favicon.png" type="image/png">
 	<title>Case-Sual</title>
 	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<link rel="stylesheet" href="vendors/linericon/style.css">
-	<link rel="stylesheet" href="css/font-awesome.min.css">
-	<link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
-	<link rel="stylesheet" href="vendors/lightbox/simpleLightbox.css">
-	<link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
-	<link rel="stylesheet" href="vendors/animate-css/animate.css">
-	<link rel="stylesheet" href="vendors/jquery-ui/jquery-ui.css">
+	<link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+	<link rel="stylesheet" href="{{ asset('vendors/linericon/style.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('vendors/owl-carousel/owl.carousel.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('vendors/lightbox/simpleLightbox.css') }}">
+	<link rel="stylesheet" href="{{ asset('vendors/nice-select/css/nice-select.css') }}">
+	<link rel="stylesheet" href="{{ asset('vendors/animate-css/animate.css') }}">
+	<link rel="stylesheet" href="{{ asset('vendors/jquery-ui/jquery-ui.css') }}">
 	<!-- main css -->
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="css/responsive.css">
+	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
 </head>
 
 <body>
@@ -31,21 +31,21 @@
 
 	<!-- Optional JavaScript -->
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="js/jquery-3.2.1.min.js"></script>
-	<script src="js/popper.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/stellar.js"></script>
-	<script src="vendors/lightbox/simpleLightbox.min.js"></script>
-	<script src="vendors/nice-select/js/jquery.nice-select.min.js"></script>
-	<script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
-	<script src="vendors/isotope/isotope-min.js"></script>
-	<script src="vendors/owl-carousel/owl.carousel.min.js"></script>
-	<script src="js/jquery.ajaxchimp.min.js"></script>
-	<script src="vendors/counter-up/jquery.waypoints.min.js"></script>
-	<script src="vendors/flipclock/timer.js"></script>
-	<script src="vendors/counter-up/jquery.counterup.js"></script>
-	<script src="js/mail-script.js"></script>
-	<script src="js/theme.js"></script>
+	<script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
+	<script src="{{ asset('js/popper.js') }}"></script>
+	<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+	<script src="{{ asset('js/stellar.js') }}"></script>
+	<script src="{{ asset('vendors/lightbox/simpleLightbox.min.js') }}"></script>
+	<script src="{{ asset('vendors/nice-select/js/jquery.nice-select.min.js') }}"></script>
+	<script src="{{ asset('vendors/isotope/imagesloaded.pkgd.min.js') }}"></script>
+	<script src="{{ asset('vendors/isotope/isotope-min.js') }}"></script>
+	<script src="{{ asset('vendors/owl-carousel/owl.carousel.min.js') }}"></script>
+	<script src="{{ asset('js/jquery.ajaxchimp.min.js') }}"></script>
+	<script src="{{ asset('vendors/counter-up/jquery.waypoints.min.js') }}"></script>
+	<script src="{{ asset('vendors/flipclock/timer.js') }}"></script>
+	<script src="{{ asset('vendors/counter-up/jquery.counterup.js') }}"></script>
+	<script src="{{ asset('js/mail-script.js') }}"></script>
+	<script src="{{ asset('js/theme.js') }}"></script>
 </body>
 
 </html>
